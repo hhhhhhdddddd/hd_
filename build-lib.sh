12c230411fb72d20b8b31e33e40b7f952de2b513
hhhhhhdddddd.com <hhhhhhdddddd.com@gmail.com>
2015-08-16 10:38:50 +0200

    Build vers le dossier build

--- build-lib.sh
+++ build-lib.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+
+# Pour conserver l'indentation
+# http://stackoverflow.com/questions/7314044/use-bash-to-read-line-by-line-and-keep-space
+IFS=''
+
+# "Build" et déploie HD_ dans les dossiers en argument en remplaçant les hd_.js existants.
 tempFile="hdtemp"
 hdFile='hd_.js'
-hdLib="../hd_.js"
+buildDir='build/'
+buildFile=$buildDir$hdFile
+
+echo "$0: deploying HD_"
+
+# Création du dossier de build s'il n'existe pas
+mkdir -p $buildDir || exit 1
+
+# Build... Juste une concaténation pour l'instant.
 find ! -name $hdFile -name "*.js" -exec cat {} \; > $tempFile
-cat $hdFile $tempFile > $hdLib
-rm $tempFile
+builtLib=$(cat $hdFile $tempFile)
+echo $builtLib > $buildFile
