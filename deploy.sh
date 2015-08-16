dfceaef556eedc1b44eac5e6240da3b04e93fcb6
hhhhhhdddddd.com <hhhhhhdddddd.com@gmail.com>
2015-08-16 10:10:52 +0200

    Build vers un dossier build puis déploiement

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-# Build et déploie hd_ dans les dossiers en argument en remplaçant les hd_.js existant
+# "Build" et déploie HD_ dans les dossiers en argument en remplaçant les hd_.js existants.
 tempFile="hdtemp"
 hdFile='hd_.js'
+buildDir='build/'
+buildFile=$buildDir$hdFile
 destinationFile="$destinationDir$hdFile"
+destinationFile="$destinationDir"
 
-echo "deploying to: $destinationFile"
+echo "$0: deploying HD_"
+
+# Création du dossier de build s'il n'existe pas
+mkdir -p $buildDir || exit 1
+
+# Build... Juste une concaténation pour l'instant.
 find ! -name $hdFile -name "*.js" -exec cat {} \; > $tempFile
+builtLib=$(cat $hdFile $tempFile)
+echo $builtLib > $buildFile
 
+# Déploiement
 for destinationDir in "$@"
 do
     destinationFile="$destinationDir$hdFile"
-    echo "$0: deploying to: $destinationFile"
-    cat $hdFile $tempFile > $destinationFile
+    echo "$0: deploying to: $destinationDir"
+    cp $buildFile  $destinationDir
 done
 
 rm $tempFile
