d5ca2ffd901caa7095e9d469faae6e8258baa5ab
hhhhhhdddddd.com <hhhhhhdddddd.com@gmail.com>
2015-08-17 15:12:27 +0200

    Modif commentaire script

--- scripts/copies.sh
+++ scripts/copies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Copie vers plusieurs dossier
-# "${@:2}"; on saute le premier argument qui est la source
+# Copie vers plusieurs dossiers
+# NB. "${@:2}"; permet de sauter le premier argument qui est la source.
 for destinationDir in "${@:2}";
 do
     echo "$0: copying $1 to $destinationDir"
