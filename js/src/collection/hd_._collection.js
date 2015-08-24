3c4af62c8e5205b1316a4562ddf2544aa203d10a
hhhhhhdddddd.com <hhhhhhdddddd.com@gmail.com>
2015-08-24 15:02:32 +0200

    Ajout de findElement aux collections

--- js/src/collection/hd_._collection.js
+++ js/src/collection/hd_._collection.js
@@ -13,6 +13,21 @@ HD_._Collection = (function() {
                 alert("HD_._Collection: clearCollection no implemented");
             };
 
+            /*
+            On profite du fait que les tableaux sont des objets
+            dont les clé sont les indices.
+            */
+            collection.findElement = function(predicat) {
+                var size = this.getSize();
+                for (var key in this._elements) {
+                    var currentElement = this.getElement(key);
+                    if (predicat(currentElement)) {
+                        return currentElement;
+                    }
+                }
+                return null;
+            };
+
             collection.getElement =  function(key) {
                 return this._elements[ key ];
             };
